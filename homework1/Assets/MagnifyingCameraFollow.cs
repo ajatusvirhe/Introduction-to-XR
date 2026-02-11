882412d4f157da5d0be64aa8922dde6727d8c90e
ajatusvirhe <164178363+ajatusvirhe@users.noreply.github.com>
2026-02-11 22:14:13 +0200

    kierto toimii (osittain) prefab lensessa

--- homework1/Assets/MagnifyingCameraFollow.cs
+++ homework1/Assets/MagnifyingCameraFollow.cs
@@ -1,11 +1,18 @@
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MagnifyingCameraFollow : MonoBehaviour
     // MAde with chatGPT
 {
     public Transform lensCenter;        // the lens center
     public Transform headCamera;  // XR Main Camera
+    Quaternion lastRotation;
 
+    private void Start()
+    {
+        lastRotation = lensCenter.rotation;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -15,11 +22,11 @@ public class MagnifyingCameraFollow : MonoBehaviour
         // Get head forward direction
         //Vector3 forward = headCamera.forward;
 
+        transform.rotation = /*Quaternion.Inverse(lastRotation)*/ lastRotation * headCamera.rotation; //transform.rot??
         // Look straight where the player looks
-        transform.rotation = headCamera.rotation;
+        //transform.rotation = headCamera.rotation;
         // Keep camera upright (no roll)
-        //transform.position += forward * 0.01f;
         //transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
-        
+        lastRotation = lensCenter.rotation;
     }
 }
